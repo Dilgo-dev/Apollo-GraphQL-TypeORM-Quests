ad0cb3cb3a87baf59e131eceb79f3972b47a602e
OOOSNA <Dilgopierre@gmail.com>
2025-03-25 12:38:45 +0100

    :rotating_light: Fix unused import

--- server/src/resolvers/cartoon.resolver.ts
+++ server/src/resolvers/cartoon.resolver.ts
@@ -1,47 +1,54 @@
-import { default as cartoons } from "../../dataset.json";
-import { Cartoon } from "../types/cartoon.type";
+import { Cartoon } from "../entities/cartoon.entities";
+import { Genre } from "../entities/genre.entities";
+import { Personnage } from "../entities/personnage.entities";
 
 type GetOneCartoonByIdArgs = {
     id: number;
 }
 
-export const getOneCartoonById = (_: unknown, args: GetOneCartoonByIdArgs): Cartoon => {
-    return cartoons.find((cartoon) => cartoon.id === +args.id) as Cartoon;
-}
-
-export const getCartoons = (): Cartoon[] => {
-    return cartoons;
-}
-
-export const createCartoon = (_: unknown, args: { cartoon: Cartoon }): number => {
-    const { cartoon } = args;
-    const newCartoonId = cartoons[cartoons.length - 1].id + 1;
-
-    cartoons.push({
-        id: newCartoonId,
-        name: cartoon.name,
-        description: cartoon.description,
-        nb_of_episodes: cartoon.nb_of_episodes,
-        nb_of_seasons: cartoon.nb_of_seasons,
-        genres: cartoon.genres,
-        realisator: cartoon.realisator,
-        author: cartoon.author,
-        ft_diffusion: cartoon.ft_diffusion,
-        personnages: cartoon.personnages.map((personnage) => ({
-            ...personnage,
-            id: new Date().getTime(),
-        })),
-    })
-    return newCartoonId;
-}
-
-
-export const deleteCartoon = (_: unknown, args: { id: string }): boolean => {
-    const { id } = args;
-    const index = cartoons.findIndex((cartoon) => cartoon.id === +id);
-    if (index === -1) {
+export const getOneCartoonById = async (_: unknown, args: GetOneCartoonByIdArgs): Promise<Cartoon> => {
+    const cartoon = await Cartoon.findOne({ where: { id: args.id } });
+    if (!cartoon) {
         throw new Error("Cartoon not found");
     }
-    cartoons.splice(index, 1);
+    return cartoon;
+}
+
+export const getCartoons = async (): Promise<Cartoon[]> => await Cartoon.find();
+
+export const createCartoon = async (_: unknown, args: { cartoon: Cartoon }): Promise<number> => {
+    const { cartoon } = args;
+
+    const newPersonnages = cartoon.personnages?.map((personnage) => {
+        const newPersonnage = new Personnage();
+        newPersonnage.name = personnage.name;
+        newPersonnage.role = personnage.role;
+        newPersonnage.short_description = personnage.short_description;
+        return newPersonnage;
+    }) as Personnage[];
+
+    const newGenres = cartoon.genres?.map((genre) => {
+        const newGenre = new Genre();
+        newGenre.name = genre.name;
+        return newGenre;
+    }) as Genre[];
+
+    const newCartoon = new Cartoon();
+    Object.assign(newCartoon, cartoon);
+    newCartoon.personnages = newPersonnages;
+    newCartoon.genres = newGenres;
+
+    const { id } = await newCartoon.save();
+    return id;
+}
+
+
+export const deleteCartoon = async (_: unknown, args: { id: string }): Promise<boolean> => {
+    const { id } = args;
+    const cartoon = await Cartoon.findOne({ where: { id: +id } });
+    if (!cartoon) {
+        throw new Error("Cartoon not found");
+    }
+    await cartoon.remove();
     return true;
 }
