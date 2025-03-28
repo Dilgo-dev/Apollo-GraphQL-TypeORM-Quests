4691ba622926b502b8e76faf19d734dfb5cded49
Dilgo-dev <dilgopierre@gmail.com>
2025-03-29 00:45:37 +0100

    :hammer: Add field to cartoon entities for type-graphql

--- server/src/entities/cartoon.entities.ts
+++ server/src/entities/cartoon.entities.ts
@@ -1,37 +1,50 @@
 import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
-import { Field, InputType } from "type-graphql";
+import { Field, InputType, ObjectType } from "type-graphql";
+
 import { Genre, GenreInput } from "./genre.entities";
 import { Personnage } from "./personnage.entities";
 import { PersonnageInput } from "./personnage.entities";
+
+@ObjectType()
 @Entity()
 export class Cartoon extends BaseEntity {
+  @Field()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   name: string;
 
+  @Field()
   @Column()
   description: string;
 
+  @Field()
   @Column()
   nb_of_episodes: number;
 
+  @Field()
   @Column()
   nb_of_seasons: number;
 
+  @Field()
   @Column()
   realisator: string;
 
+  @Field()
   @Column()
   author: string;
 
+  @Field()
   @Column()
   ft_diffusion: string;
 
+  @Field(() => [Genre])
   @OneToMany(() => Genre, (genre) => genre.cartoons)
   genres?: Genre[];
 
+  @Field(() => [Personnage])
   @OneToMany(() => Personnage, (personnage) => personnage.cartoon)
   personnages?: Personnage[];
 }
