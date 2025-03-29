f2f5bb501634712c86ec283ec50b1df7b5dc41d6
Dilgo-dev <dilgopierre@gmail.com>
2025-03-29 01:03:29 +0100

    :sparkles: Configure apollo server to work with type-graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,56 +1,23 @@
 /** Import des librairies */
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { buildSchema } from "type-graphql";
 import "reflect-metadata";
 
 import CartoonResolver from "./resolvers/cartoon.resolver";
-import { Personnage, PersonnageInput } from "./schemas/personnage.schema";
-import { Cartoon, CartoonInput } from "./schemas/cartoon.schema";
 import { dataSource } from "./database/client";
 
-const cartoonResolver = new CartoonResolver();
-
-const typeDefs = `#graphql
-  # This "Cartoon" type defines the queryable fields for every cartoon in our data source.
-  type Cartoon ${Cartoon}
-  type Personnage ${Personnage}
-  input PersonnageInput ${PersonnageInput}
-  input CartoonInput ${CartoonInput}
-
-  # The "Query" type is special: it lists all of the available queries
-  type Query {
-    getCartoons: [Cartoon],
-    getOneCartoonById(id: ID!): Cartoon
-  }
-
-  type Mutation {
-    createCartoon(cartoon: CartoonInput!): ID
-    deleteCartoon(id: ID!): Boolean
-  }
-`;
-
-// This resolver retrieves books from the "books" array above.
-const resolvers = {
-    Query: {
-      getCartoons: cartoonResolver.getCartoons,
-      getOneCartoonById: cartoonResolver.getOneCartoonById,
-    },
-    Mutation: {
-      createCartoon: cartoonResolver.createCartoon,
-      deleteCartoon: cartoonResolver.deleteCartoon,
-    },
-  };
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
 
 /** Fonction auto appellée (évite la mise en constante) permettant de lancer le serveur */
 (async () => {
   await dataSource.initialize();
+
+  const schema = await buildSchema({
+    resolvers: [CartoonResolver],
+  });
+
+  const server = new ApolloServer({ schema });
+
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
   });
